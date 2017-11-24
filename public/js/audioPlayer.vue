cdfbd5d866a5523d9f0e33306b7b96fba06c44f7
EZPK <thom.bruyer@gmail.com>
2017-11-24 05:46:56 +0100

    real data + data binding from vue instance to components

--- public/js/audioPlayer.vue
+++ public/js/audioPlayer.vue
@@ -1,5 +1,8 @@
+// v-bind:src=
+// Permet de récupérer le nom dans une url sans qu'il soit interprêté par les 
+
 Vue.component('song', {
-  props: ['songName', 'songTitle'],  
+  props: ['songName', 'songTitle', 'songPath'],  
   template: `
   <div class="player">
     <div class="playerTitle">
@@ -7,7 +10,7 @@ Vue.component('song', {
     </div>
     <p class="songTitle">{{ songTitle }}</p>
     <audio id="music">
-      <source src="http://localhost:7777/music?id=music1.mp3"/>
+      <source v-bind:src="'http://localhost:7777/music?id=' + songPath"/>
       Your browser does not support the audio element.
     </audio>
     <div id="timeline">
@@ -19,13 +22,14 @@ Vue.component('song', {
 })
 
 
-let song = new Vue({
+new Vue({
   el:"#songs",
   data:{
     songs:[
       { id:0, title:"Kids with Gunz", author:"Gorillaz", album:"Demon Days", imgSrc:"/img/gorillaz.png", path:"music1.mp3" },
       { id:1, title:"Polynomial C", author:"Afx", album:"Analord", imgSrc:"/img/afx.png", path:"music2.mp3" },
-      { id:2, title:"Mad Villain", author:"MF DOOM", album:"Mad Villany", imgSrc:"/img/mfdoom.png", path:"music3.mp3" },  
+      { id:2, title:"Accordion", author:"MF DOOM", album:"Mad Villany", imgSrc:"/img/mfdoom.png", path:"music3.mp3" },  
+      { id:3, title:"Abbesse", author:"Birdy Nam Nam", album:"", imgSrc:"/img/birdy.png", path:"music4.mp3" },        
     ]
   }
 })
