9353dc1f0297b673685b4cebdc4e4437ba5e6866
EZPK <thom.bruyer@gmail.com>
2017-11-24 04:17:38 +0100

    Multiple component from the same Vue with different data

--- public/js/audioPlayer.vue
+++ public/js/audioPlayer.vue
@@ -1,68 +1,31 @@
-let data = [
-  {
-    id:0,
-    title:"Kids with Gunz",
-    author:"Gorillaz",
-    album:"Demon Days",
-    imgSrc:"/img/gorillaz.png",
-    path:"music1.mp3"
-  },
-  {
-    id:1,
-    title:"Polynomial C",
-    author:"Afx",
-    album:"Analord",
-    imgSrc:"/img/afx.png",
-    path:"music2.mp3"
-  }
-]
-
-Vue.component('player-component', {
+Vue.component('song', {
+  props: ['songName', 'songTitle'],  
   template: `
   <div class="player">
-    <div>
-      <div class="playerTitle">
-        <h2 class="songAuthor">{{ data[0].author }}</h2>      
-      </div>
-      <p class="songTitle">{{ data[0].title }}</p>
-      <audio id="music">
-        <source src="http://localhost:7777/music?id=music1.mp3"/>
-        Your browser does not support the audio element.
-      </audio>
-      <div id="timeline">
-        <div id="playhead"></div>
-      </div>
-      <button id="pButton" class="play"></button>      
+    <div class="playerTitle">
+      <h2 class="songAuthor">{{ songName }}</h2>      
     </div>
-  </div>	
-  `,
-  data: {
-    path: data[0].path
-  }
-})
-
-Vue.component('player-component2', {
-  template: `
-  <div class="player">
-    <div>
-      <div class="playerTitle">
-        <h2 class="songAuthor">{{ data[1].author }}</h2>
-      </div>      
-      <p class="songTitle">{{ data[1].title }}</p>
-      <audio id="music">
-        <source src="http://localhost:7777/music?id=music1.mp3"/>
-        Your browser does not support the audio element.
-      </audio>
-      <div id="timeline">
-        <div id="playhead"></div>
-      </div>
-      <button id="pButton" class="play"></button>      
+    <p class="songTitle">{{ songTitle }}</p>
+    <audio id="music">
+      <source src="http://localhost:7777/music?id=music1.mp3"/>
+      Your browser does not support the audio element.
+    </audio>
+    <div id="timeline">
+      <div id="playhead"></div>
     </div>
+    <button id="pButton" class="play"></button>      
   </div>	
   `
 })
 
-new Vue({
-  el:"#app",
-  data: data
+
+let song = new Vue({
+  el:"#songs",
+  data:{
+    songs:[
+      { id:0, title:"Kids with Gunz", author:"Gorillaz", album:"Demon Days", imgSrc:"/img/gorillaz.png", path:"music1.mp3" },
+      { id:1, title:"Polynomial C", author:"Afx", album:"Analord", imgSrc:"/img/afx.png", path:"music2.mp3" },
+      { id:2, title:"Mad Villain", author:"MF DOOM", album:"Mad Villany", imgSrc:"/img/mfdoom.png", path:"music3.mp3" },  
+    ]
+  }
 })
